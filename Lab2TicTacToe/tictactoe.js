96b1dc9fbebe7e625d8ec6bce836f8c9b1b8de7e
Rachel Fernandez <ferna185@csusm.edu>
2019-07-25 16:19:38 -0700

    TICTACTOE

---
+++ Lab2TicTacToe/tictactoe.js
@@ -0,0 +1,54 @@
+const box1 = document.querySelector('#box1');
+box1.addEventListener('CLICK', (event) => {
+  console.log('Turn 1')
+  box1.innerHTML = 'X';
+  box1.style.backgroundColor = 'red';
+});
+
+
+
+
+
+
+
+
+
+// const box2 = document.querySelector('#box1');
+// box2.addEventListener('CLICK', (event) => {
+//   console.log('Turn 1')
+// }
+//
+// const box3 = document.querySelector('#box1');
+// box3.addEventListener('CLICK', (event) => {
+//   console.log('Turn 1')
+// }
+//
+// const box4 = document.querySelector('#box1');
+// box4.addEventListener('CLICK', (event) => {
+//   console.log('Turn 1')
+// }
+//
+// const box5 = document.querySelector('#box1');
+// box5.addEventListener('CLICK', (event) => {
+//   console.log('Turn 1')
+// }
+//
+// const box6 = document.querySelector('#box1');
+// box6.addEventListener('CLICK', (event) => {
+//   console.log('Turn 1')
+// }
+//
+// const box7 = document.querySelector('#box1');
+// box7.addEventListener('CLICK', (event) => {
+//   console.log('Turn 1')
+// }
+//
+// const box8 = document.querySelector('#box1');
+// box8.addEventListener('CLICK', (event) => {
+//   console.log('Turn 1')
+// }
+//
+// const box9 = document.querySelector('#box1');
+// box9.addEventListener('CLICK', (event) => {
+//   console.log('Turn 1')
+// };
